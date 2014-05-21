428ec954bd90299d134fcc0bba668ff2763ee111
Juan Tejería <jt.tejeria@gmail.com>
2014-05-21 20:56:37 -0300

    Implementacion getMenu
    Se implemento el metido que obtiene el menu de un restaurant

--- BUS/src/dao/LocalDao.java
+++ BUS/src/dao/LocalDao.java
@@ -1,9 +1,20 @@
 package dao;
 
+import java.util.LinkedList;
+
 import dao.exception.DaoException;
 import dao.exception.NoDataFoundException;
 import entity.Shop;
+import entity.plate;
 
 public interface LocalDao {
+	
+	//Obtener un Local por su nombre
 	Shop getByName(String name) throws NoDataFoundException, DaoException;
+	
+	//Obtener el Menu de un restaurante por el nombre del restaurante.
+	LinkedList<plate> getMenu(String name) throws NoDataFoundException, DaoException;
+	
+	
+	
 }
