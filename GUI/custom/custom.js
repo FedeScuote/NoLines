bf6cd5359401e21757ee3c52c576440c7591759c
Federico Scuoteguazza <fscuoteguazza@gmail.com>
2014-05-23 12:48:09 -0300

    Added JSON Restaurant
    GUI uses a JSON service that gives a restaurant list

--- GUI/custom/custom.js
+++ GUI/custom/custom.js
@@ -2,8 +2,35 @@
  * Created by Fede on 19/05/2014.
  */
 function deviceReady() {
-    $("#click2").on("click", function (e) {
-        mui.viewPort.showPage("mui-viewport-page2", "SLIDE_LEFT");
+    $("#click2").on("click", function(e) {
+        $.ajax({
+            url:'http://www.portones.com.uy/shopsite/AppService',
+            crossDomain: true,
+            data:{
+                cmdaction:'locales',
+                shopping:'ms',
+                web:2
+            }
+            /*url:'http://localhost:8080/WebServices/RestaurantServiceImpl',
+            crossDomain: true,
+            data:{
+            }*/
+        })
+            .done(function(data){
+                var restaurantes=JSON.parse(data);
+                var ret = '';
+                for(var i = 0 ; i < restaurantes.length ; i++){
+                    ret.concat('<li><img src=restaurantes[i].image><div> restaurantes[i].name<br> </div></li>')
+                }
+                $("#restaurant-list").html(ret);
+                mui.viewPort.iScrollRefresh();
+                mui.viewPort.showPage("mui-viewport-page2", "SLIDE_LEFT");
+
+            })
+            .fail(function(jqXHR, textStatus, errorThrown){
+
+            })
     });
+
 
 }
