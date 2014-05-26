aaa141795020ead406c67ec4871e88a49f82b2c9
FedeScuoteguazza <FedeScuoteguazza@Fede-PC>
2014-05-26 00:27:01 -0300

    added new window menu selector, added Qunit module

--- GUI/custom/custom.js
+++ GUI/custom/custom.js
@@ -1,19 +1,17 @@
-/**
- * Created by Fede on 19/05/2014.
- */
 function deviceReady() {
     $("#click2").on("click", function(e) {
         $.ajax({    
             url:'http://localhost:8080/WebServices/RestaurantServiceServlet',
             crossDomain: true,
             data:{
+                ws: 1
             }
         })
             .done(function(data){
                 var obj=JSON.parse(data);
                 var ret = "";
                 for(var i = 0 ; i < obj.length ; i++){
-                	ret = ret + "<li class='restaurant-selector'><button class='mui-clickable default-button'><img src="
+                	ret = ret + "<li class='restaurant-selector'><button class='mui-clickable default-button' onclick='restaurantSelection("+obj[i].id+")'><img src="
                 	+obj[i].logo+" width='50' height='50'><div class='restaurant-title'> "+obj[i].name+"<br> </div>" +
                         "<div class='restaurant-info'>"+obj[i].location+"<br>"+obj[i].horario+"</div>" +
                         "</button></li>";
@@ -27,6 +25,31 @@ function deviceReady() {
 
             })
     });
+
+function restaurantSelection(param) {
+        $.ajax({
+            url:'http://localhost:8080/WebServices/RestaurantServiceServlet',
+            crossDomain: true,
+            data:{
+                ws: 2,
+                idRestaurant: param
+            }
+        })
+            .done(function(data){
+                var obj=JSON.parse(data);
+                var ret = "";
+                for(var i = 0 ; i < obj.length ; i++){
+                    ret = ret + "<li class='menu-selector'> obj[i]</li>";
+                }
+                $("#restaurant-list").html(ret);
+                mui.viewPort.iScrollRefresh();
+                mui.viewPort.showPage("mui-viewport-page3", "SLIDE_LEFT");
+
+            })
+            .fail(function(jqXHR, textStatus, errorThrown){
+
+            })
+    };
 
 
 }
