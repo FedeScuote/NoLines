d0515a82c7df2d135116568d100fd6fec6eddfbb
FedeScuoteguazza <FedeScuoteguazza@Fede-PC>
2014-05-26 01:20:37 -0300

    added retrieve menus function for each restaurant, shows a title showing it works

--- GUI/custom/custom.js
+++ GUI/custom/custom.js
@@ -1,19 +1,19 @@
 function deviceReady() {
-    $("#click2").on("click", function(e) {
-        $.ajax({    
-            url:'http://localhost:8080/WebServices/RestaurantServiceServlet',
+    $("#click2").on("click", function (e) {
+        $.ajax({
+            url: 'http://localhost:8080/WebServices/RestaurantServiceServlet',
             crossDomain: true,
-            data:{
+            data: {
                 ws: 1
             }
         })
-            .done(function(data){
-                var obj=JSON.parse(data);
+            .done(function (data) {
+                var obj = JSON.parse(data);
                 var ret = "";
-                for(var i = 0 ; i < obj.length ; i++){
-                	ret = ret + "<li class='restaurant-selector'><button class='mui-clickable default-button' onclick='restaurantSelection("+obj[i].id+")'><img src="
-                	+obj[i].logo+" width='50' height='50'><div class='restaurant-title'> "+obj[i].name+"<br> </div>" +
-                        "<div class='restaurant-info'>"+obj[i].location+"<br>"+obj[i].horario+"</div>" +
+                for (var i = 0; i < obj.length; i++) {
+                    ret = ret + "<li class='restaurant-selector'><button class='mui-clickable default-button' onclick=restaurantSelection(" + obj[i].id + ")><img src="
+                        + obj[i].logo + " width='50' height='50'><div class='restaurant-title'> " + obj[i].name + "<br> </div>" +
+                        "<div class='restaurant-info'>" + obj[i].location + "<br>" + obj[i].horario + "</div>" +
                         "</button></li>";
                 }
                 $("#restaurant-list").html(ret);
@@ -21,11 +21,11 @@ function deviceReady() {
                 mui.viewPort.showPage("mui-viewport-page2", "SLIDE_LEFT");
 
             })
-            .fail(function(jqXHR, textStatus, errorThrown){
+            .fail(function (jqXHR, textStatus, errorThrown) {
 
             })
     });
-
+}
 function restaurantSelection(param) {
         $.ajax({
             url:'http://localhost:8080/WebServices/RestaurantServiceServlet',
@@ -39,9 +39,9 @@ function restaurantSelection(param) {
                 var obj=JSON.parse(data);
                 var ret = "";
                 for(var i = 0 ; i < obj.length ; i++){
-                    ret = ret + "<li class='menu-selector'> obj[i]</li>";
+                    ret = ret + "<li class='menu-selector'>"+ obj[i].logo+"</li>";
                 }
-                $("#restaurant-list").html(ret);
+                $("#menu-list").html(ret);
                 mui.viewPort.iScrollRefresh();
                 mui.viewPort.showPage("mui-viewport-page3", "SLIDE_LEFT");
 
@@ -49,7 +49,6 @@ function restaurantSelection(param) {
             .fail(function(jqXHR, textStatus, errorThrown){
 
             })
-    };
-
-
 }
+
+
