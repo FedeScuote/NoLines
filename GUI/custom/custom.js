b89e9a40f7ac4e9e943266b7b16a5a4128aa5667
Juan Tejería <jt.tejeria@gmail.com>
2014-06-01 00:11:01 -0300

    Messages
    New messages when no restaurant.

--- GUI/custom/custom.js
+++ GUI/custom/custom.js
@@ -10,12 +10,17 @@ function deviceReady() {
             .done(function (data) {
                 var obj = JSON.parse(data);
                 var ret = "";
-                for (var i = 0; i < obj.length; i++) {
-                    ret = ret + "<li class='restaurant-selector'><button class='mui-clickable default-button' onclick=restaurantSelection(" + obj[i].id + ")><img src="
-                        + obj[i].logo + " width='50' height='50'><div class='restaurant-title'> " + obj[i].name + "<br> </div>" +
-                        "<div class='restaurant-info'>" + obj[i].location + "<br>" + obj[i].horario + "</div>" +
-                        "</button></li>";
+                if(obj.length > 0){
+                	for (var i = 0; i < obj.length; i++) {
+                        ret = ret + "<li class='restaurant-selector'><button class='mui-clickable default-button' onclick=restaurantSelection(" + obj[i].id + ")><img src="
+                            + obj[i].logo + " width='50' height='50'><div class='restaurant-title'> " + obj[i].name + "<br> </div>" +
+                            "<div class='restaurant-info'>" + obj[i].location + "<br>" + obj[i].horario + "</div>" +
+                            "</button></li>";
+                    }
+                }else{
+                	ret = "No hay restaurantes disponibles, revise su coneccion a internet. Si el problema persiste, puede deberse a un error en la aplicacion. Saludos de NoLines team.";
                 }
+                
                 $("#restaurant-list").html(ret);
                 mui.viewPort.iScrollRefresh();
                 mui.viewPort.showPage("mui-viewport-page2", "SLIDE_LEFT");
